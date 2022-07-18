2831d85c958b80366f1aed7e723f04e56298d00f
Michel-Freitas <michelfreitas85@outlook.com>
2022-07-18 00:48:12 -0300

    Implementing and styling the Home Page

--- front/src/views/Home/index.tsx
+++ front/src/views/Home/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import InputText from "../../components/InputText";
+import MovieCard from "./MovieCard";
+import "./style.scss";
 
 const Home: React.FC = () => {
     return (
         <div className="home">
-            Home
+            <InputText
+                name="searchFilms"
+                placeholder="Pesquisar..."
+            />
+            <main className="home__main">
+                <MovieCard />
+                <MovieCard />
+                <MovieCard />
+                <MovieCard />
+                <MovieCard />
+                <MovieCard />
+                <MovieCard />
+                <MovieCard />
+                <MovieCard />
+                <MovieCard />
+            </main>
         </div>
     )
 }
