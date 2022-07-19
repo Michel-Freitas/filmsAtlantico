beb62f4eedfa82fb3dded7a494e4e47a716998c4
Michel-Freitas <michelfreitas85@outlook.com>
2022-07-19 17:15:33 -0300

    Refactoring the Modal component and consuming the API

--- front/src/views/Home/index.tsx
+++ front/src/views/Home/index.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import InputText from "../../components/InputText";
+import { IFilm } from "../../schemas/FilmSchemas";
+import { useFilmsService } from "../../service";
 import MovieCard from "./MovieCard";
 import "./style.scss";
 
 const Home: React.FC = () => {
+    
+    const { VITE_BASE_URL_IMAGES } = import.meta.env;
+    const filmService = useFilmsService();
+    const [filmsList, setFilmsList] = useState<IFilm[]>([]);
+
+    useEffect(() => {
+        initial();
+    }, []);
+
+    async function initial() {
+        const result = await filmService.getFilms();
+        setFilmsList(result);
+    }
+
     return (
         <div className="home">
             <InputText
@@ -11,16 +27,18 @@ const Home: React.FC = () => {
                 placeholder="Pesquisar..."
             />
             <main className="home__main">
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
+                {filmsList.map((item) => {
+                    return <MovieCard
+                        key={item.id}
+                        coverUrl={`${VITE_BASE_URL_IMAGES}${item.coverPhoto}`}
+                        detailsButtonAction={() => {}}
+                        favoriteButtonAction={() => {}}
+                        movieTitle={item.title}
+                        releaseDate={item.releaseDate}
+                        movieId={item.id}
+                        isFavorite={item.favorite}
+                    />
+                })}
             </main>
         </div>
     )
